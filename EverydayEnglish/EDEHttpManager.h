7c99c0aebedc1f97c2c67aa9fda270d05cc118cd
APP <app@APP.local>
2014-12-14 13:18:53 +0800

    创建网络管理类文件。

---
+++ EverydayEnglish/EDEHttpManager.h
@@ -0,0 +1,13 @@
+//
+//  EDEHttpManager.h
+//  EverydayEnglish
+//
+//  Created by APP on 14/12/1.
+//  Copyright (c) 2014年 ll. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EDEHttpManager : NSObject
+
+@end
---
+++ EverydayEnglish/EDEHttpManager.m
@@ -0,0 +1,13 @@
+//
+//  EDEHttpManager.m
+//  EverydayEnglish
+//
+//  Created by APP on 14/12/1.
+//  Copyright (c) 2014年 ll. All rights reserved.
+//
+
+#import "EDEHttpManager.h"
+
+@implementation EDEHttpManager
+
+@end
